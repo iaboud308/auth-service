0578263cb06dcec4e5362844a67eefe8062b8996
Ibrahim <iaboud308@gmail.com>
2024-09-16 12:59:44 +0100

    deploy

--- controllers/healthController.go
+++ controllers/healthController.go
@@ -7,5 +7,5 @@ import (
 // HealthCheck simply returns a message indicating the service is up
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Service is up and running"))
+	w.Write([]byte("Auth service is up and running"))
 }
