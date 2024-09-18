c66f923edf2885e3270428a7c14b225cc2ea8562
Ibrahim <iaboud308@gmail.com>
2024-09-18 15:53:43 +0100

    deploy

--- models/user.go
+++ models/user.go
@@ -2,11 +2,13 @@ package models
 
 // User struct representing the user entity
 type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"-"`
-	System   int    `json:"system"`
-	Role     string `json:"role"`
-	Hospital int    `json:"hospital"`
-	Status   string `json:"status"` // 'pending', 'approved', or 'declined'
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	System    int    `json:"system"`
+	Role      string `json:"role"`
+	Hospital  int    `json:"hospital"`
+	Status    string `json:"status"` // 'pending', 'approved', or 'declined'
 }
