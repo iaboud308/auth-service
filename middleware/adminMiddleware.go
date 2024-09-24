8c043247fd9c051f55374d7ed741f6cada4f5563
Ibrahim <iaboud308@gmail.com>
2024-09-24 16:54:11 +0100

    deploy

--- middleware/adminMiddleware.go
+++ middleware/adminMiddleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"auth-service/services"
+	"log"
 	"net/http"
 )
 
@@ -10,7 +11,11 @@ func RequireAdmin(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tokenStr := r.Header.Get("Authorization")
 		user, err := services.GetUserFromToken(tokenStr)
-		if err != nil || user.Role != "admin" {
+
+		log.Println("RequireAdmin Controller User", user, "Error", err)
+
+		if err != nil || user.Role != "Admin" {
+			log.Println("RequireAdmin Controller User: Will not pass")
 			http.Error(w, "Forbidden", http.StatusForbidden)
 			return
 		}
