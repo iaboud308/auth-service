29b2cd099e92d5a4d4a25521d6bee6ce235e8edf
Ibrahim <iaboud308@gmail.com>
2024-10-29 12:10:51 +0000

    deploy

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	router := routes.SetupRouter()
 
 	// CORS settings
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "X-System-Id", "X-Tenant-Id", "Content-Type", "Authorization"})
 	originsOk := handlers.AllowedOrigins([]string{"http://localhost:3000"})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
 
